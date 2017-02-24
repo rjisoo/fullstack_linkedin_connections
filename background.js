b19a3ce757668e309f3c3d9a5829a0ac7db7c355
Sia <schen22@gmail.com>
2017-02-24 14:31:08 -0500

    connecting and closes windows on already connected people

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
 let url_arr =
-  ['https://www.linkedin.com/in/lisa-pan',
-  'https://www.linkedin.com/in/damonye',
-  'https://www.linkedin.com/in/schen22',
-  'https://www.linkedin.com/in/rjisoo']
+  ['https://www.linkedin.com/in/james-dicolandrea/',
+  'https://www.linkedin.com/in/mb3311/',
+  'https://www.linkedin.com/in/reicolee/',
+  'https://www.linkedin.com/in/chloemrice/'
+  // 'https://www.linkedin.com/in/andres-de-jesus-80566054/'
+  // 'https://www.linkedin.com/in/anujshah333'
+  // 'https://www.linkedin.com/in/schen22',
+  // 'https://www.linkedin.com/in/rjisoo'
+  ]
 
-
-url_arr.forEach( url => {
-  chrome.tabs.create({url: url}, function(tab) {
-    chrome.tabs.executeScript(null, {file: "content.js"});
-  })
+url_arr.forEach((url, i) => {
+  setTimeout(function() {
+    chrome.tabs.create({url: url}, function(tab) {
+      chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+        chrome.tabs.executeScript(null, {file: "content.js"});
+      })
+    });
+  }, 10000 * i);
 })
--- content.js
+++ content.js
@@ -1,9 +1,47 @@
-window.onLoad = () => {
+$(document).ready(() => {
   console.log("WHAT???")
-  document.getElementsByTagName('span').filter()
-    "span:contains('Connect')").trigger('click');
-  setTimeout(function() {
-    console.log("INSIDE HERE")
-    jQuery("button:contains('Send now')").trigger('click');
-  }, 1000);
+  let $connect = $("span:contains('Connect')");
+
+  $connect.on('click', function() {
+    console.log("just clicked connect")
+    setTimeout(() => {
+      let $sendnow = $("button:contains('Send now')");
+      $sendnow.on('click', function() {
+        console.log("about to close")
+        window.close();
+      })
+      $sendnow.trigger('click');
+    }, 1000);
+  })
+  console.log("CONNNN", $connect)
+  if ($connect.length === 0) {
+    window.close();
+  } else {
+    $connect.trigger('click');
+  }
 })
+
+  // $connect.trigger('click');
+  // setTimeout(function() {
+  //   console.log("HERE YO")
+  //   .trigger('click');
+  //   setTimeout(() => {
+  //     console.log("TIME OUT")
+  //     window.close();
+  //   }, 1000);
+  // }, 1000);
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   //do work
+//   console.log("DO YOU RUN?")
+// });
+
+// window.document.onload = () => {
+//   console.log("DUDE WHY?")
+//   var spans = document.getElementsByTagName("span");
+//   for (let i = 0; i < spans.length; i++){
+//     console.log("INSIDE FOR")
+//       if (spans[i].textContent == 'Connect') {
+//           spans[i].click();
+//       }
+//   }
+// }
