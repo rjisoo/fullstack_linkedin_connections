bce3edbbb4a11a957e9cc73827ac5909939aa98b
Sia <schen22@gmail.com>
2017-02-24 13:31:39 -0500

    messy

--- content.js
+++ content.js
@@ -1,4 +1,9 @@
-setTimeout(function() {
-  $("span:contains('Connect')").trigger('click');
-}, 1000);
-$("button:contains('Send now')").trigger('click');
+window.onLoad = () => {
+  console.log("WHAT???")
+  document.getElementsByTagName('span').filter()
+    "span:contains('Connect')").trigger('click');
+  setTimeout(function() {
+    console.log("INSIDE HERE")
+    jQuery("button:contains('Send now')").trigger('click');
+  }, 1000);
+})
