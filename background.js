f74661570bb9f9191d155b97c993f56fcfe686c4
Lisa Pan <l.pan4952@gmail.com>
2017-02-24 12:58:45 -0500

    initialized

---
+++ background.js
@@ -0,0 +1,9 @@
+let url_arr =
+  ['https://www.linkedin.com/in/lisa-pan',
+  'https://www.linkedin.com/in/damonye',
+  'https://www.linkedin.com/in/schen22',
+  'https://www.linkedin.com/in/rjisoo']
+
+url_arr.forEach( url => {
+
+})
