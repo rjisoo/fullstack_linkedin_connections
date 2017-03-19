c0fc72fe11b15fe3bd6b594fa844c5ba915aacea
dye784 <damon.ye91@gmail.com>
2017-03-18 20:22:43 -0400

    removed comments and fixed formatting

--- content.js
+++ content.js
@@ -17,28 +17,3 @@ $(document).ready(() => {
     $connect.trigger('click');
   }
 })
-
-  // $connect.trigger('click');
-  // setTimeout(function() {
-  //   console.log("HERE YO")
-  //   .trigger('click');
-  //   setTimeout(() => {
-  //     console.log("TIME OUT")
-  //     window.close();
-  //   }, 1000);
-  // }, 1000);
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   //do work
-//   console.log("DO YOU RUN?")
-// });
-
-// window.document.onload = () => {
-//   console.log("DUDE WHY?")
-//   var spans = document.getElementsByTagName("span");
-//   for (let i = 0; i < spans.length; i++){
-//     console.log("INSIDE FOR")
-//       if (spans[i].textContent == 'Connect') {
-//           spans[i].click();
-//       }
-//   }
-// }
